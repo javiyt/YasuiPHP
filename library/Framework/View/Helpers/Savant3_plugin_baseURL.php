9b9a5014b7ea94a1defd49d379572f9dd533fd04
Meth <mth@mthweb.org>
2009-09-07 16:30:32 +0200

    Cambiando todo de directorio

--- library/Framework/View/Helpers/Savant3_plugin_baseURL.php
+++
@@ -1,9 +0,0 @@
-<?php
-class Savant3_plugin_baseURL extends Savant3_plugin
-{
-    public function baseURL()
-    {
-        return substr($_SERVER['SCRIPT_NAME'], 0, strrpos($_SERVER['SCRIPT_NAME'], "/")+1);
-    }
-}
-
